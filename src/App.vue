ea7de0bbf7213f16144310dd4b301787ffcc4605
sisoubou <serena.fadda8@gmail.com>
2026-02-17 14:18:36 +0100

    Création architecture

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="min-h-screen bg-gray-900 text-white p-6">
+    <h1 class="text-3xl font-bold">Tailwind v4 OK</h1>
+  </div>
 </template>
+
 
 <style scoped></style>
